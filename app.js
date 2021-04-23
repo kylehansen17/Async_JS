f48dab4771dc2dba94529b941d487e161dc6fd52
kylehansen17 <kyle.hansen925@gmail.com>
2021-04-23 07:34:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -56,3 +56,50 @@ delayedColorChange('red', 1000)
     .then(() => delayedColorChange('blue', 1000))
     .then(() => delayedColorChange('indigo', 1000))
     .then(() => delayedColorChange('violet', 1000))
+
+
+// Async Funtions
+
+// Async Keyword
+function example() {
+    // regular funcion
+}
+async function example() {
+    // makes a promise
+}
+
+const login = async (username, password) => {
+    if (!username || !password) throw 'Missing Credentials'
+    if (password === 'thepassword') return 'Welcome'
+    throw 'Invalid Password'
+}
+
+login('sdkfjlajf', 'thepassword')
+    .then(msg => {
+        console.log('Logged In')
+        console.log(msg)
+    })
+    .catch(err => {
+        console.log('Error')
+        console.log(err)
+    })
+
+
+// Await Keyword
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('orange', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('violet', 1000))
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+}
